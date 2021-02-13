7780706459d1aa0a1e61e1dc0e135159a95ed098
Carlo Landmeter <clandmeter@alpinelinux.org>
2021-02-13 14:38:46 +0100

    entrypoint: make profile configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 : ${PACKAGER:=alpinelinux}
 : ${MIRROR:=https://dl-cdn.alpinelinux.org/alpine}
 : ${RELEASE:=latest-stable}
+: ${PROFILE:=netboot}
 
 abuild-keygen -ain
 
@@ -11,7 +12,7 @@ git clone --depth=1 git://git.alpinelinux.org/aports
 find /tmp -type f -name "*.patch" | xargs -n1 git -C ~/aports apply
 
 cd aports/scripts && ./mkimage.sh \
-	--profile netboot \
+	--profile $PROFILE \
 	--repository "$MIRROR/$RELEASE"/main \
 	--outdir /root/out
 
